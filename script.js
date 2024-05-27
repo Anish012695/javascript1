b1507213da1cf0b8ebe69142c641979aae982f54
Anish012695 <anishmukharjee20578@gmail.com>
2024-05-27 16:30:29 +0530

    27-05-2024

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// var obj={
+//     name:"Anish",
+//     class:"MCA",
+//     Roll:8,
+//     age:24
+// }
+// // Object.freeze(obj);
+// obj.age=25;
+// function anish(){
+//     return "hello world"
+// }
+
+// var anis=anish();
+// const fs =require('fs');
+// fs.rename("hey.txt","hello.txt",function(err){
+//     if (err) console.error(err);
+//     else console.log("done");
+// })
+// 
+// fs.writeFile("hey.txt","hi main hu na",function(err){
+//     if (err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.copyFile("hello.txt","./copy/chacha.txt",function(err){
+//     if (err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.unlink("hello.txt",function(err){
+//     if(err) console.err(err);
+//     else console.log("removed");
+// });
+
+// fs.rmdir("./copy",{recursive:true},function(err){
+//     if(err) console.error(err);
+//     else console.log("removed");
+// })
+// const http=require('http');
+// const server =http.createServer(function(req,res){
+//     res.end("helo world");
+// })
+// server.listen(3000);
+
+
+
